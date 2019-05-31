e7db45f2d1ab26b9670e453a7f56efdddc39e150
Craig A Wiswell <cwiswell63@gmail.com>
2019-05-31 18:51:54 -0400

    Add Test for draw card from payer hand and add card to hand

--- WarCardGame.Test/PlayerTests.cs
+++ WarCardGame.Test/PlayerTests.cs
@@ -30,5 +30,26 @@ namespace WarCardGame.Test
 
             Assert.False(player.ActivePlayer);
         }
+
+        [Fact]
+        public void DrawCardFromEmptyHand()
+        {
+            var player = new Player();
+
+            var drawnCard = player.DrawCard();
+
+            Assert.Null(drawnCard);
+        }
+
+        [Fact]
+        public void AddCardToPlayerHand()
+        {
+            var player = new Player();
+
+            var cardToAdd = new Card(CardSuiteEnum.Clubs, CardValueEnum.Ace);
+            player.AddCard(cardToAdd);
+
+            Assert.True(player.GetHandCount() == 1);
+        }
     }
 }
