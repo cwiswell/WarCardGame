052e61aaf002dacf916995c33b31e44a2c6ff009
Craig A Wiswell <cwiswell63@gmail.com>
2019-06-02 09:23:07 -0400

    Add tests for card == operator

--- WarCardGame.Test/CardTests.cs
+++ WarCardGame.Test/CardTests.cs
@@ -50,5 +50,41 @@ namespace WarCardGame.Test
 
             Assert.False(newCard1.Equals(newCard2));
         }
+
+        [Fact]
+        public void TwoCardsEqualOperator()
+        {
+            var newCard1 = new Card(CardSuiteEnum.Heart, CardValueEnum.Queen);
+            var newCard2 = new Card(CardSuiteEnum.Heart, CardValueEnum.Queen);
+
+            Assert.True(newCard1 == newCard2);
+        }
+
+        [Fact]
+        public void TwoCardsValuesEqualOperator()
+        {
+            var newCard1 = new Card(CardSuiteEnum.Heart, CardValueEnum.Queen);
+            var newCard2 = new Card(CardSuiteEnum.Diamond, CardValueEnum.Queen);
+
+            Assert.True(newCard1 == newCard2);
+        }
+
+        [Fact]
+        public void TwoCardsValuesDifferentEqualOperator()
+        {
+            var newCard1 = new Card(CardSuiteEnum.Heart, CardValueEnum.Queen);
+            var newCard2 = new Card(CardSuiteEnum.Heart, CardValueEnum.Two);
+
+            Assert.False(newCard1 == newCard2);
+        }
+
+        [Fact]
+        public void TwoCardsValuesAndSuiteDifferentEqualOperator()
+        {
+            var newCard1 = new Card(CardSuiteEnum.Heart, CardValueEnum.Queen);
+            var newCard2 = new Card(CardSuiteEnum.Diamond, CardValueEnum.Two);
+
+            Assert.False(newCard1 == newCard2);
+        }
     }
 }
