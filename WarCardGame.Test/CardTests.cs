49394367f0a8e4f294bd525a7841b0d38739e4ef
Craig A Wiswell <cwiswell63@gmail.com>
2019-06-02 09:13:16 -0400

    Add Card Tests to test various functions on card class

---
+++ WarCardGame.Test/CardTests.cs
@@ -0,0 +1,46 @@
+﻿using WarCardGame.Models;
+using Xunit;
+
+namespace WarCardGame.Test
+{
+    public class CardTests
+    {
+        [Fact]
+        public void EmptyCardToString()
+        {
+            var newCard = new Card();
+
+            var cardString = newCard.ToString();
+
+            Assert.True(cardString == "Two of Spade");
+        }
+
+        [Fact]
+        public void CardToString()
+        {
+            var newCard = new Card(CardSuiteEnum.Heart, CardValueEnum.Queen);
+
+            var cardString = newCard.ToString();
+
+            Assert.True(cardString == "Queen of Heart");
+        }
+
+        [Fact]
+        public void TwoCardsAreEqual()
+        {
+            var newCard1 = new Card(CardSuiteEnum.Heart, CardValueEnum.Queen);
+            var newCard2 = new Card(CardSuiteEnum.Heart, CardValueEnum.Queen);
+
+            Assert.True(newCard1.Equals(newCard2));
+        }
+
+        [Fact]
+        public void TwoCardsSuitesAreNotEqual()
+        {
+            var newCard1 = new Card(CardSuiteEnum.Heart, CardValueEnum.Queen);
+            var newCard2 = new Card(CardSuiteEnum.Diamond, CardValueEnum.Queen);
+
+            Assert.False(newCard1.Equals(newCard2));
+        }
+    }
+}
